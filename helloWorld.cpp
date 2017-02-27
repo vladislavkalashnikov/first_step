245b87438dbb63e5df7a1cd73f976a3fabe6cd0e
vladislav kalashnikov <vladik.vladislav@gmail.com>
2017-02-27 15:37:32 +0300

    comment several lines

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[]) {
 
-    double d = 4.51 ;
+/*    double d = 4.51 ;
     int i = 7 ;
     cout << "d = "<< d << endl;
     cout << "i = " << i << endl;
@@ -12,7 +12,7 @@ int main(int argc, char* argv[]) {
     cout << "d + i= " << d << endl;
     i = (int) (d * i);
     cout << "i * d =" << i << endl;
-
+*/
 
 	std::cout << "Hello, World!"<< std::endl;
 	return 0;
