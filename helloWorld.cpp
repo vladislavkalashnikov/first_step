6774fc0268e3f2cb7eb38d99b76ef971b13c7135
vladislav kalashnikov <vladik.vladislav@gmail.com>
2017-02-27 14:14:41 +0300

    Add helloWorld.cpp

---
+++ helloWorld.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+ using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    double d = 4.51 ;
+    int i = 7 ;
+    cout << "d = "<< d << endl;
+    cout << "i = " << i << endl;
+    d = d + i;
+    cout << "d + i= " << d << endl;
+    i = (int) (d * i);
+    cout << "i * d =" << i << endl;
+
+
+	std::cout << "Hello, World!"<< std::endl;
+	return 0;
+}
